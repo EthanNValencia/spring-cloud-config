866bd18ac65f53022ca7e140af7ac4ba90cb9e17
EthanNValencia <enephew2@mail.valenciacollege.edu>
2023-07-10 14:42:12 -0400

    Just committing changes.

--- src/main/java/com/nephew/microservices/springcloudconfig/SpringCloudConfigApplication.java
+++ src/main/java/com/nephew/microservices/springcloudconfig/SpringCloudConfigApplication.java
@@ -2,10 +2,15 @@ package com.nephew.microservices.springcloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigApplication {
 
+	// Properties can viewed at
+	// http://localhost:8888/limits-service/default
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigApplication.class, args);
 	}
